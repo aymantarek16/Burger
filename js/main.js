a22ffc48f3629de61db9173f9dbf5eff16da5158
aymantarek16 <aymantarek16@yahoo.com>
2022-04-17 04:55:28 +0200

    add scroll active link

--- js/main.js
+++ js/main.js
@@ -15,16 +15,37 @@ window.addEventListener("scroll", () => {
   mynavbar.classList.toggle("mynavbar2", scrollY > 0);
 });
 
-
 // Show NavLinks When Click On BtnToggler
-let navbarToggler = document.querySelector(".navbar-toggler")
-let navbarCollapse = document.querySelector(".navbar-collapse") 
+let navbarToggler = document.querySelector(".navbar-toggler");
+let navbarCollapse = document.querySelector(".navbar-collapse");
 
+navbarToggler.addEventListener("click", () => {
+  navbarCollapse.classList.toggle("collapse");
+});
 
-navbarToggler.addEventListener("click" , ()=>{
-  navbarCollapse.classList.toggle("collapse")
-})
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav_item a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav_item a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
 //  ==================================== Start Jquery ====================================
 $(document).ready(function () {
